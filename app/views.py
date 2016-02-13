5c58255705d83c1fdc333c64170a1b8fff1d8934
Camilo Nova <camilo.nova@axiacore.com>
2016-02-13 08:58:25 -0500

    :sunny: Added cache to sentry and mmonit

--- app/views.py
+++ app/views.py
@@ -38,7 +38,7 @@ class SpotligthView(LoginRequiredMixin, View):
 
 class TickerView(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-        response = []
+        response_list = []
 
         # Zendesk
         zendesk_data = cache.get('zendesk_data')
@@ -55,38 +55,53 @@ class TickerView(LoginRequiredMixin, View):
                 }
                 cache.set('zendesk_data', zendesk_data, 60)
 
-        response.append(zendesk_data)
+        response_list.append(zendesk_data)
 
         # Sentry
-        req = requests.get(settings.SENTRY_URL, auth=(settings.SENTRY_KEY, ''))
-        if req.ok:
-            response.append({
-                'title': 'Events',
-                'label': 'Sentry',
-                'value': sum([x[1] for x in req.json()]),
-            })
+        sentry_data = cache.get('sentry_data')
+        if not sentry_data:
+            req = requests.get(
+                settings.SENTRY_URL,
+                auth=(settings.SENTRY_KEY, ''),
+            )
+            if req.ok:
+                sentry_data = {
+                    'title': 'Events',
+                    'label': 'Sentry',
+                    'value': sum([x[1] for x in req.json()]),
+                }
+                cache.set('sentry_data', sentry_data, 60)
+
+        response_list.append(sentry_data)
 
         # Mmonit
-        s = requests.Session()
-        s.get(settings.MMONIT_URL + 'index.csp')
-        s.post(
-            settings.MMONIT_URL + 'z_security_check',
-            params={
-                'z_username': settings.MMONIT_USER,
-                'z_password': settings.MMONIT_PASS,
-            }
-        )
-        req = s.post(
-            settings.MMONIT_URL + 'reports/uptime/list', params={'range': '6'}
-        )
-        if req.ok:
-            for item in req.json()['items']:
-                response.append({
-                    'title': item['name'],
-                    'label': 'Uptime',
-                    'value': '{0}%'.format(item['uptime']),
-                })
+        mmonit_list = cache.get('mmonit_list')
+        if not mmonit_list:
+            s = requests.Session()
+            s.get(settings.MMONIT_URL + 'index.csp')
+            s.post(
+                settings.MMONIT_URL + 'z_security_check',
+                params={
+                    'z_username': settings.MMONIT_USER,
+                    'z_password': settings.MMONIT_PASS,
+                }
+            )
+            req = s.post(
+                settings.MMONIT_URL + 'reports/uptime/list',
+                params={'range': '6'},
+            )
+            if req.ok:
+                mmonit_list = []
+                for item in req.json()['items']:
+                    mmonit_list.append({
+                        'title': item['name'],
+                        'label': 'Uptime',
+                        'value': '{0}%'.format(item['uptime']),
+                    })
+                cache.set('mmonit_list', mmonit_list, 90)
+
+        response_list += mmonit_list
 
         return JsonResponse({
-            'list': response,
+            'list': response_list,
         })
