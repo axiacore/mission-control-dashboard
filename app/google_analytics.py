4f7f9b67b05fc0b13296a8e642fafe5c1d9adb4d
Santiago Feijoo <santiago.feijoo@axiacore.com>
2016-02-17 08:35:19 -0500

    :lollipop: Updated oauth2client library

--- app/google_analytics.py
+++ app/google_analytics.py
@@ -2,20 +2,12 @@ import json
 
 from django.conf import settings
 
-from oauth2client.client import SignedJwtAssertionCredentials
+from oauth2client.service_account import ServiceAccountCredentials
 
 
-# Load the key file's private data.
-with open(settings.CLIENT_SECRET_FILE) as key_file:
-    _key_data = json.load(key_file)
-
-
-# Construct a credentials objects from the key data and OAuth2 scope.
-SCOPE = 'https://www.googleapis.com/auth/analytics.readonly'
-_credentials = SignedJwtAssertionCredentials(
-    _key_data['client_email'],
-    _key_data['private_key'],
-    SCOPE,
+_credentials = ServiceAccountCredentials.from_json_keyfile_name(
+    settings.CLIENT_SECRET_FILE,
+    scopes=['https://www.googleapis.com/auth/analytics.readonly'],
 )
 
 
