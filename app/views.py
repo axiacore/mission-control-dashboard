1c358c386c3c34bbb72ebf3542abd6097fe23b37
Camilo Nova <camilo.nova@axiacore.com>
2016-02-25 09:05:52 -0500

    :sunny: Handle connection error

--- app/views.py
+++ app/views.py
@@ -14,6 +14,7 @@ from .models import GoogleAnalyticsSite
 from .models import GoogleAnalyticsSiteGoal
 
 import requests
+from requests.exceptions import ConnectionError
 
 
 spotligth_cycle = cycle('AB')
@@ -53,64 +54,76 @@ class TickerView(LoginRequiredMixin, View):
         # Zendesk
         zendesk_data = cache.get('zendesk_data')
         if not zendesk_data:
-            req = requests.get(
-                settings.ZENDESK_URL,
-                auth=(settings.ZENDESK_EMAIL, settings.ZENDESK_API),
-            )
-            if req.ok:
-                zendesk_data = {
-                    'title': 'Tickets',
-                    'label': 'Zendesk',
-                    'value': req.json()['view_count']['value'],
-                }
-                cache.set('zendesk_data', zendesk_data, 60)
+            try:
+                req = requests.get(
+                    settings.ZENDESK_URL,
+                    auth=(settings.ZENDESK_EMAIL, settings.ZENDESK_API),
+                )
+                if req.ok:
+                    zendesk_data = {
+                        'title': 'Tickets',
+                        'label': 'Zendesk',
+                        'value': req.json()['view_count']['value'],
+                    }
+                    cache.set('zendesk_data', zendesk_data, 120)
+            except ConnectionError:
+                zendesk_data = None
 
-        response_list.append(zendesk_data)
+        if zendesk_data:
+            response_list.append(zendesk_data)
 
         # Sentry
         sentry_data = cache.get('sentry_data')
         if not sentry_data:
-            req = requests.get(
-                settings.SENTRY_URL,
-                auth=(settings.SENTRY_KEY, ''),
-            )
-            if req.ok:
-                sentry_data = {
-                    'title': 'Events',
-                    'label': 'Sentry',
-                    'value': sum([x[1] for x in req.json()]),
-                }
-                cache.set('sentry_data', sentry_data, 60)
+            try:
+                req = requests.get(
+                    settings.SENTRY_URL,
+                    auth=(settings.SENTRY_KEY, ''),
+                )
+                if req.ok:
+                    sentry_data = {
+                        'title': 'Events',
+                        'label': 'Sentry',
+                        'value': sum([x[1] for x in req.json()]),
+                    }
+                    cache.set('sentry_data', sentry_data, 60)
+            except ConnectionError:
+                sentry_data = None
 
-        response_list.append(sentry_data)
+        if sentry_data:
+            response_list.append(sentry_data)
 
         # Mmonit
         mmonit_list = cache.get('mmonit_list')
         if not mmonit_list:
-            s = requests.Session()
-            s.get(settings.MMONIT_URL + 'index.csp')
-            s.post(
-                settings.MMONIT_URL + 'z_security_check',
-                params={
-                    'z_username': settings.MMONIT_USER,
-                    'z_password': settings.MMONIT_PASS,
-                }
-            )
-            req = s.post(
-                settings.MMONIT_URL + 'reports/uptime/list',
-                params={'range': '6'},
-            )
-            if req.ok:
-                mmonit_list = []
-                for item in req.json()['items']:
-                    mmonit_list.append({
-                        'title': item['name'],
-                        'label': 'Uptime',
-                        'value': '{0}%'.format(item['uptime']),
-                    })
-                cache.set('mmonit_list', mmonit_list, 90)
+            try:
+                s = requests.Session()
+                s.get(settings.MMONIT_URL + 'index.csp')
+                s.post(
+                    settings.MMONIT_URL + 'z_security_check',
+                    params={
+                        'z_username': settings.MMONIT_USER,
+                        'z_password': settings.MMONIT_PASS,
+                    }
+                )
+                req = s.post(
+                    settings.MMONIT_URL + 'reports/uptime/list',
+                    params={'range': '6'},
+                )
+                if req.ok:
+                    mmonit_list = []
+                    for item in req.json()['items']:
+                        mmonit_list.append({
+                            'title': item['name'],
+                            'label': 'Uptime',
+                            'value': '{0}%'.format(item['uptime']),
+                        })
+                    cache.set('mmonit_list', mmonit_list, 90)
+            except ConnectionError:
+                mmonit_list = None
 
-        response_list += mmonit_list
+        if mmonit_list:
+            response_list += mmonit_list
 
         return render(request, 'ticker_detail.html', {
             'response_list': response_list,
